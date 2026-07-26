2d3f23bb471240b93aa27a0fa84d0892c3c97744
Micath73 <mintesnottesfaye139@gmail.com>
2026-07-26 17:29:17 +0300

    feat(luminous): add full Rosary mystery sequence with custom images and translations

--- lib/glorious.dart
+++ lib/glorious.dart
@@ -90,24 +90,193 @@ class _gloriousScreenState extends State<gloriousScreen> {
                         titleAm: 'በስመ አብ ወወልድ ወመንፈስ ቅዱስ',
                         contentEn: 'In The Name Of The Father,\nAnd Of The Son,\nAnd Of The Holy Spirit.\nAmen',
                         contentAm: 'በስመ አብ ወወልድ ወመንፈስ ቅዱስ አሐዱ አምላክ አሜን።',
-                        imagePath: 'assets/wmremove-transformed (10).png',
+                        imagePath: 'assets/wmremove-transformed (9).jpeg',
                       ),
                       PrayerStep(
                         sectionHeader: 'Apostles\' Creed',
                         titleEn: 'The Apostles\' Creed',
                         titleAm: 'የሐዋርያት እምነት',
                         contentEn: 'I believe in God, the Father almighty, Creator of heaven and earth, and in Jesus Christ, His only Son, our Lord, who was conceived by the Holy Spirit, born of the Virgin Mary, suffered under Pontius Pilate, was crucified, died and was buried; He descended into hell; on the third day He rose again from the dead; He ascended into heaven, and seated at the right hand of God, the Father almighty; from thence He shall come to judge, the living and the dead. I believe in the Holy Spirit, the holy Catholic Church, the communion of saints, the forgiveness of sins, the resurrection of the body and life everlasting.\nAmen!',
-                        contentAm: 'ሁሉን በሚችል በሰማይና በምድር ፈጣሪ በእግዚአብሔር አብ አመናለሁ...',
-                        imagePath: 'assets/wmremove-transformed (10).png',
+                        contentAm: 'ሁሉን በሚችል ሰማይንና ምድርን በፈጠረ\nበእግዚአብሔር አብ አምናለሁ፡፡\nአንድ ልጁ በሆነዉ በጌታችን\nበኢየሱስ ክርስቶስ አምናለሁ፤\nእሱ በመንፋስ ቅዱስ ተጸንሶ\nከድንግል ማርያም ተወለደ፡፡\nበጴንጤናዊው ጲላጦስ ዘመን ተሰቃየ፣\nተሰቀለ፣ ሞተ፣ ተቀበረ፣\nወደ ሲኦል ወረደ፣\nበሶስተኛው ቀን ከሙታን ተለይቶ ተነሣ፣\nወደ ሰማይ ወጣ፣\nሁሉን በሚችል በእግዚአብሔር አብ ቀኝ ተቀመጠ፣\nያሉትንና የሞቱትን ሊፈርድ ዳግመኛ ይመጣል፣\n በመንፈስ ቅዱስም አምናለሁ፣\nየሁሉም በሆነችው በቅድስት ቤተክርስቲያን\nበቅዱሳንም አንድነት አምናለሁ፣\nበኃጢአት ስርየት፣\nበሙታን ትንሣኤ፣\nበዘላለም ሕይወት አምናለሁ፡፡\nአሜን!',
+                        imagePath: 'assets/wmremove-transformed (9).jpeg',
                       ),
                       PrayerStep(
                         sectionHeader: 'Our Father',
                         titleEn: 'Our Father',
                         titleAm: 'አባታችን ሆይ',
-                        contentEn: 'Our Father, who art in heaven, hallowed be Thy name...',
-                        contentAm: 'አባታችን ሆይ፤ በሰማያት የምትኖር፤ ስምህ ይቀደስ...',
-                        imagePath: 'assets/wmremove-transformed (10).png',
+                        contentEn: 'Our Father, who art in heaven, hallowed be thy name; thy kingdom come; thy will be done on earth as it is in heaven. Give us this day our daily bread; and forgive us our trespasses as we forgive those who trespass against us; and lead us not into temptation, but deliver us from evil.\nAmen!',
+                        contentAm: 'በሰማይ የምትኖር አባታችን ሆይ፣ስምህ ይመስገን፣መንግሥትህ ትምጣ፡ፈቃድህ በሰማይ እንደሆነ:እንዲሁም በምድር ይሁን። የዕለት እንጀራችንን ዛሬ ስጠን፤እኛ የበደሉንን ይቅር እንደምንል:በደላችንን ይቅር በልልን፤ከክፉ ሁሉ ሰውርን እንጂ ወደ ፈተና አታግባን፡፡ መንግስት ኃይልና ክብር ለዘለዓለም ያንተ ነውና፡፡\nአሜን!',
+                        imagePath: 'assets/wmremove-transformed (9).jpeg',
                       ),
+                      PrayerStep(
+                        sectionHeader: 'Hail Mary',
+                        titleEn: 'Hail Mary',
+                        titleAm: 'ጸጋ የመላሽ',
+                        contentEn: 'Hail Mary, full of grace, the Lord is with you; Blessed art thou among women, and blessed is the fruit of thy womb, Jesus. Holy Mary, Mother of God, pray for us sinners now and at the hour of our death.\nAmen!',
+                        contentAm: 'ጸጋ የመላሽ ማርይም ሆይ:ሰላም ላንቺ ይሁን፣እግዚአብሔር ካንቺ ጋር ነው፣ከሴቶች ሁሉ የተባረክሽ ነሽ፣የሆድሽም ፍሬ ኢየሱስ የተባረከ ነው፡፡ ቅድስት ማርያም የአምላክ እናት:ለኛ ለኃጢአተኞች:አሁንም በሞታችንም ጊዜ ለምኝልን፡፡\nአሜን።',
+                        imagePath: 'assets/wmremove-transformed (9).jpeg',
+                      ),
+                      PrayerStep(
+                        sectionHeader: 'Glory Be',
+                        titleEn: 'Glory Be',
+                        titleAm: 'ስብሐት ለአብ',
+                        contentEn: 'Glory be to the Father,\nand to the Son,\nand to the Holy Spirit;\nas it was in the beginning,\nis now, and ever shall be,\nworld without end.\nAmen!',
+                        contentAm: 'ለአብ:ለወልድና:ለመንፈስ ቅዱስ ምስጋና ይሁን\nአሁንም ዘወትርም ለዘለዓለምም::\n አሜን!"',
+                        imagePath: 'assets/wmremove-transformed (9).jpeg',
+                      ),
+                      PrayerStep(
+                        sectionHeader: 'Fatima Prayer',
+                        titleEn: 'Fatima Prayer',
+                        titleAm: 'የፋጢማ ጸሎት',
+                        contentEn:'O my Jesus,\nforgive us our sins,\nsave us from the fires of hell;\nlead all souls to Heaven,\nespecially those who have\nmost need of your mercy.\nAmen.',
+                        contentAm:'አቤቱ ጌታዬ ኢየሱስ ሆይ፡\nኃጢአታችንን ይቅር በለን፡\nከገሃነም እሳት አድነን፡\nነፍሳትን ሁሉ ወደ መንግሥተ ሰማያት አግባ። አሜን።',
+                        imagePath: 'assets/wmremove-transformed (9).jpeg',
+                      ),
+                      PrayerStep(
+                          titleEn: "First Glorious Mystery",
+                          titleAm: "አንደኛ የክብር ምስጢር",
+                          contentEn: "The Resurrection.\n\n"
+                              "\"But on the first day of the week, at early dawn,\n"
+                              "they went to the tomb, taking the spices which they had prepared.\n"
+                              "And they found the stone rolled away from the tomb,\n"
+                              "but when they went in they did not find the body.\n"
+                              "While they were perplexed about this,\n"
+                              "behold, two men stood by them in dazzling apparel;\n"
+                              "and as they were frightened and bowed their faces to the ground,\n\n"
+                              "the men said to them,\n"
+                              "'Why do you seek the living among the dead? He is not here, but has risen'\n"
+                              "(Lk 24:1-5)\n\n\n"
+                              "Our Father...\n"
+                              "10 Hail Marys\n"
+                              "Glory be to the Father...\n"
+                              "Fatima Prayer...",
+                          contentAm: "ጌታችን ኢየሱስ ክርስቶስ፡\n"
+                              "ሞቶ ከተቀበረ በኋላ በሶስተኛው ቀን፡\n"
+                              " ስለእኛ ኃጢአት እያሰበ ደም መላቡን ማሰብ ነው፡፡\n"
+                              "ከሙታን መነሳቱን፡\n"
+                              "እንዲሁም ሁለተኛ የማይሞት መሆኑን ማሰብ ነው፡፡\n"
+                              "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                              "ከኃጢአት ተነሥቶ እንደገና ወደ ኃጢአት አለመመለስ ነው፡፡\n\n\n"
+                              "አባታችን ሆይ...\n"
+                              "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                              "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                              "የፋጢማ ጸሎት...\n",
+                          imagePath: "assets/img_27.png"
+                      ),
+
+                      PrayerStep(
+                        titleEn: "Second Glorious Mystery",
+                        titleAm: "ሁለተኛ የክብር ምስጢር",
+                        contentEn: "The Ascension.\n\n"
+                            "\"So then the Lord Jesus,\n"
+                            "after he had spoken to them,\n"
+                            "was taken up into heaven,\n"
+                            " and sat down at the right hand of God.\n"
+                            "(Mk 16:19).\n\n\n"
+
+                            "Our Father...\n"
+                            "10 Hail Marys\n"
+                            "Glory be to the Father...\n"
+                            "Fatima Prayer...",
+                        contentAm: "ጌታችን ኢየሱስ ክርስቶስ፡\n"
+                            "ከሙታን በተነሣ በአርባኛው ቀን፡\n"
+                            "በታላቅ ክብር ወደ ሰማይ መውጣቱን ማሰብ ነው፡፡\n"
+                            "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                            "ሰማይን ዘወትር ማሰብና መመኘት ነው፡፡\n\n\n"
+
+                            "አባታችን ሆይ...\n"
+                            "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                            "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                            "የፋጢማ ጸሎት...\n",
+
+                        imagePath: "assets/img_28.png",
+                      ),
+
+                      PrayerStep(
+                        titleEn: "Third Glorious Mystery",
+                        titleAm: "ሶስተኛ የክብር ምስጢር",
+                        contentEn: "The descent of the Holy Spirit.\n\n"
+                            "\"When the day of Pentecost had come,\n"
+                            "they were all together in one place.\n"
+                            "And suddenly a sound came from heaven,\n"
+                            "like the rush of a mighty wind,\n"
+                            "and it filled all the house where they were sitting.\n"
+                            "And there appeared to them\n tongues as of fire,\n"
+                            "distributed and resting on\n each one of them.\n\n"
+                            "And they were all filled\n with the Holy Spirit,\n"
+                            "and began to speak in other tongues,\n"
+                            "as the Spirit gave them utterance.\n"
+                            "(Acts 2:1-4).\n\n\n"
+                            "Our Father...\n"
+                            "10 Hail Marys\n"
+                            "Glory be to the Father...\n"
+                            "Fatima Prayer...",
+                        contentAm: "ሐዋርያት ከእመቤታችን ጋር፡\n"
+                            "በኢየሩሳሌም ተሰብስበው ሲጸልዩ፡\n"
+                            "በሃምሳኛው ቀን መንፈስ ቅዱስ መውረዱን ማሰብ ነው፡፡\n"
+                            "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                            "የእግዚሰብሔርን ፍቅር ለማግኘት፡\n"
+                            "መንፈስ ቅዱስን መለመን ነው፡፡\n\n\n"
+
+                            "አባታችን ሆይ...\n"
+                            "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                            "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                            "የፋጢማ ጸሎት...\n",
+                        imagePath: "assets/img_29.png",
+                      ),
+
+                      PrayerStep(
+                        titleEn: "The Annunciation",
+                        titleAm: "አራተኛ የክብር ምስጢር",
+                        contentEn: "The descent of the Holy Spirit.\n\n"
+                            "\"Henceforth all generations will call me blessed;\n"
+                            "for he who is mighty has done great things for me.\n"
+                            "(Lk 1:48-49).\n\n\n"
+
+                            "Our Father...\n"
+                            "10 Hail Marys\n"
+                            "Glory be to the Father...\n"
+                            "Fatima Prayer...",
+                        contentAm: "እመቤታችን ቅድስት ድንግል ማርያም፡\n"
+                            "በእግዚአብሔር ኃይል ወደ ሰማይ መውጣትዋን ማሰብ ነው፡፡\n"
+                            "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                            "መልካም ሞት ለመሞት፡\n"
+                            "በየቀኑ ማርያምን መለመን ነው፡፡\n\n\n"
+
+                            "አባታችን ሆይ...\n"
+                            "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                            "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                            "የፋጢማ ጸሎት...\n",
+                        imagePath: "assets/img_30.png",
+                      ),
+
+                      PrayerStep(
+                        titleEn: "Fifth Glorious Mystery",
+                        titleAm: "አምስተኛ የክብር ምስጢር",
+                        contentEn: "The crowning of Our Lady Queen of Heaven.\n\n"
+                            "\"And a great portent appeared in heaven,\n"
+                            "a woman clothed with the sun,\n"
+                            "with the moon under her feet,\n"
+                            "and on her head a crown of twelve stars.\n"
+                            "(Rev 12:1 ).\n\n\n"
+
+                            "Our Father...\n"
+                            "10 Hail Marys\n"
+                            "Glory be to the Father...\n"
+                            "Fatima Prayer...",
+                        contentAm: "እመቤታችን ቅድስት ድንግል ማርያም፡\n"
+                            "ከእግዚአብሔር የክብር አክሊል ማግኘትዋንና፡\n"
+                            "የሰማይና የምድር ንግሥት መሆኑዋን ማሰብ ነው፡፡\n"
+                            "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                            "እመቤታችን ማርያምን ዕለት ዕለት መቁጠሪያ እየደገምን ማክበር ነው፡፡\n\n\n"
+
+                            "አባታችን ሆይ...\n"
+                            "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                            "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                            "የፋጢማ ጸሎት...\n",
+                        imagePath: "assets/img_31.png",
+                      )
+
                     ];
 
                     // 2. Pass the list and current language setting into prayer_session
--- lib/joyful.dart
+++ lib/joyful.dart
@@ -98,7 +98,7 @@ class _joyfulScreenState extends State<joyfulScreen> {
                       titleAm: 'በስመ አብ ወወልድ ወመንፈስ ቅዱስ',
                       contentEn: 'In The Name Of The Father,\nAnd Of The Son,\nAnd Of The Holy Spirit.\nAmen',
                       contentAm: 'በስመ አብ ወወልድ ወመንፈስ ቅዱስ አሐዱ አምላክ አሜን።',
-                      imagePath: 'assets/wmremove-transformed (10).png',
+                      imagePath: 'assets/wmremove-transformed (3).jpeg',
                     ),
                     PrayerStep(
                       sectionHeader: 'Apostles\' Creed',
@@ -106,7 +106,7 @@ class _joyfulScreenState extends State<joyfulScreen> {
                       titleAm: 'የሐዋርያት እምነት',
                       contentEn: 'I believe in God, the Father almighty, Creator of heaven and earth, and in Jesus Christ, His only Son, our Lord, who was conceived by the Holy Spirit, born of the Virgin Mary, suffered under Pontius Pilate, was crucified, died and was buried; He descended into hell; on the third day He rose again from the dead; He ascended into heaven, and seated at the right hand of God, the Father almighty; from thence He shall come to judge, the living and the dead. I believe in the Holy Spirit, the holy Catholic Church, the communion of saints, the forgiveness of sins, the resurrection of the body and life everlasting.\nAmen!',
                       contentAm: 'ሁሉን በሚችል ሰማይንና ምድርን በፈጠረ\nበእግዚአብሔር አብ አምናለሁ፡፡\nአንድ ልጁ በሆነዉ በጌታችን\nበኢየሱስ ክርስቶስ አምናለሁ፤\nእሱ በመንፋስ ቅዱስ ተጸንሶ\nከድንግል ማርያም ተወለደ፡፡\nበጴንጤናዊው ጲላጦስ ዘመን ተሰቃየ፣\nተሰቀለ፣ ሞተ፣ ተቀበረ፣\nወደ ሲኦል ወረደ፣\nበሶስተኛው ቀን ከሙታን ተለይቶ ተነሣ፣\nወደ ሰማይ ወጣ፣\nሁሉን በሚችል በእግዚአብሔር አብ ቀኝ ተቀመጠ፣\nያሉትንና የሞቱትን ሊፈርድ ዳግመኛ ይመጣል፣\n በመንፈስ ቅዱስም አምናለሁ፣\nየሁሉም በሆነችው በቅድስት ቤተክርስቲያን\nበቅዱሳንም አንድነት አምናለሁ፣\nበኃጢአት ስርየት፣\nበሙታን ትንሣኤ፣\nበዘላለም ሕይወት አምናለሁ፡፡\nአሜን!',
-                      imagePath: 'assets/wmremove-transformed (10).png',
+                      imagePath: 'assets/wmremove-transformed (3).jpeg',
                     ),
                     PrayerStep(
                       sectionHeader: 'Our Father',
@@ -114,7 +114,7 @@ class _joyfulScreenState extends State<joyfulScreen> {
                       titleAm: 'አባታችን ሆይ',
                       contentEn: 'Our Father, who art in heaven, hallowed be thy name; thy kingdom come; thy will be done on earth as it is in heaven. Give us this day our daily bread; and forgive us our trespasses as we forgive those who trespass against us; and lead us not into temptation, but deliver us from evil.\nAmen!',
                       contentAm: 'በሰማይ የምትኖር አባታችን ሆይ፣ስምህ ይመስገን፣መንግሥትህ ትምጣ፡ፈቃድህ በሰማይ እንደሆነ:እንዲሁም በምድር ይሁን። የዕለት እንጀራችንን ዛሬ ስጠን፤እኛ የበደሉንን ይቅር እንደምንል:በደላችንን ይቅር በልልን፤ከክፉ ሁሉ ሰውርን እንጂ ወደ ፈተና አታግባን፡፡ መንግስት ኃይልና ክብር ለዘለዓለም ያንተ ነውና፡፡\nአሜን!',
-                      imagePath: 'assets/wmremove-transformed (10).png',
+                      imagePath: 'assets/wmremove-transformed (3).jpeg',
                     ),
                     PrayerStep(
                       sectionHeader: 'Hail Mary',
@@ -122,7 +122,7 @@ class _joyfulScreenState extends State<joyfulScreen> {
                       titleAm: 'ጸጋ የመላሽ',
                       contentEn: 'Hail Mary, full of grace, the Lord is with you; Blessed art thou among women, and blessed is the fruit of thy womb, Jesus. Holy Mary, Mother of God, pray for us sinners now and at the hour of our death.\nAmen!',
                       contentAm: 'ጸጋ የመላሽ ማርይም ሆይ:ሰላም ላንቺ ይሁን፣እግዚአብሔር ካንቺ ጋር ነው፣ከሴቶች ሁሉ የተባረክሽ ነሽ፣የሆድሽም ፍሬ ኢየሱስ የተባረከ ነው፡፡ ቅድስት ማርያም የአምላክ እናት:ለኛ ለኃጢአተኞች:አሁንም በሞታችንም ጊዜ ለምኝልን፡፡\nአሜን።',
-                      imagePath: 'assets/wmremove-transformed (10).png',
+                      imagePath: 'assets/wmremove-transformed (3).jpeg',
                     ),
                     PrayerStep(
                       sectionHeader: 'Glory Be',
@@ -130,7 +130,7 @@ class _joyfulScreenState extends State<joyfulScreen> {
                       titleAm: 'ስብሐት ለአብ',
                       contentEn: 'Glory be to the Father,\nand to the Son,\nand to the Holy Spirit;\nas it was in the beginning,\nis now, and ever shall be,\nworld without end.\nAmen!',
                       contentAm: 'ለአብ:ለወልድና:ለመንፈስ ቅዱስ ምስጋና ይሁን\nአሁንም ዘወትርም ለዘለዓለምም::\n አሜን!"',
-                      imagePath: 'assets/wmremove-transformed (10).png',
+                      imagePath: 'assets/wmremove-transformed (3).jpeg',
                     ),
                     PrayerStep(
                       sectionHeader: 'Fatima Prayer',
@@ -138,8 +138,182 @@ class _joyfulScreenState extends State<joyfulScreen> {
                       titleAm: 'የፋጢማ ጸሎት',
                       contentEn:'O my Jesus,\nforgive us our sins,\nsave us from the fires of hell;\nlead all souls to Heaven,\nespecially those who have\nmost need of your mercy.\nAmen.',
                       contentAm:'አቤቱ ጌታዬ ኢየሱስ ሆይ፡\nኃጢአታችንን ይቅር በለን፡\nከገሃነም እሳት አድነን፡\nነፍሳትን ሁሉ ወደ መንግሥተ ሰማያት አግባ። አሜን።',
-                      imagePath: 'assets/wmremove-transformed (10).png',
+                      imagePath: 'assets/wmremove-transformed (3).jpeg',
                     ),
+                    PrayerStep(
+                        titleEn: "First Joyful Mystery",
+                        titleAm: "አንደኛ የደስታ ምስጢር",
+                        contentEn: "The Annunciation.\n\n"
+                            "\"In the sixth month\n"
+                            "the angel Gabriel was sent from God\n"
+                            "to a city of Galilee named Nazareth,\n"
+                            "to a virgin betrothed to a man whose name was Joseph, of the house of David;\n"
+                            "and the virgin's name was Mary\n"
+                            "(Lk 1:26-27).\n\n\n"
+
+                            "Our Father...\n"
+                            "10 Hail Marys\n"
+                            "Glory be to the Father...\n"
+                            "Fatima Prayer...",
+                        contentAm: "ቅዱስ ገብርኤል\n"
+                            "ከሰማይ መጥቶ ለድንግል ማርያም እንዲህ አላት፡፡\n"
+                            "ጸጋ የሞላሽ ሰላም ላንቺ!\n"
+                            "ወንድ ልጅ ትወልጅያለሽ\n"
+                            " ስሙንም ኢየሱስ ትይዋለሽ::\n"
+                            "እመቤታችንም እንደ ቃልህ ይሁንልኝ አለች፡፡\n"
+                            "በዚያ ጊዜ ኢየሱስ በመንፈስ ቅዱስ ኃይል መፀነሱንና\n"
+                            "ለኛ ሰው መሆኑን ማሰብ ነው፡፡\n"
+                            "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                            "ትዕብትን መናቅና ትህትናን መለመን፡፡\n\n\n"
+
+                            "አባታችን ሆይ...\n"
+                            "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                            "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                            "የፋጢማ ጸሎት...\n",
+                        imagePath: "assets/img_32.png"
+                    ),
+
+                    PrayerStep(
+                        titleEn: "Second Joyful Mystery",
+                        titleAm: "ሁለተኛ የደስታ ምስጢር",
+                        contentEn: "The Visitation.\n\n"
+                            "\"In those days\n"
+                            "Mary arose and went with haste into the hill country,\n"
+                            "to a city of Judah,\n"
+                            "and she entered the house of Zechariah and greeted Elizabeth.\n"
+                            "And when Elizabeth heard the greeting of Mary,\n"
+                            "the babe leaped in her womb;\n"
+                            "and Elizabeth was filled with the Holy Spirit\n"
+                            "and she exclaimed with a loud cry,\n"
+                            "'Blessed are you among women, and blessed is the fruit of your womb!'\n"
+                            "(Lk 1:39-42).\n\n\n"
+
+                            "Our Father...\n"
+                            "10 Hail Marys\n"
+                            "Glory be to the Father...\n"
+                            "Fatima Prayer...",
+                        contentAm: "እመቤታችን ድንግል ማርያም\n"
+                            " የቅድስት ኤልሳቤጥን መፅነስ በሰማች ጊዜ\n"
+                            "ልትጎበኛት ወደ እሷ መሄድዋን:\n"
+                            " በእርስዋም ዘንድ ሶስት ወር መቀመጧን ማሰብ ነው፡፡\n"
+                            "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                            "የሰውን ፍቅር መለመንና ለማግኘት መትጋት ነው፡፡\n\n\n"
+
+                            "አባታችን ሆይ...\n"
+                            "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                            "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                            "የፋጢማ ጸሎት...\n",
+                        imagePath: "assets/img_33.png"
+                    ),
+
+                    PrayerStep(
+                        titleEn: "Third Joyful Mystery",
+                        titleAm: "ሶስተኛ የደስታ ምስጢር",
+                        contentEn: "The Birth of Our Lord.\n\n"
+                            "\"In those days\n"
+                            "a decree went out from Caesar Augustus\n"
+                            "that all the world should be enrolled.\n"
+                            "This was the first enrolment, when Quirinius was governor of Syria.\n"
+                            "And all went to be enrolled, each to his own city.\n"
+                            "And Joseph also went up from Galilee,\n"
+                            "from the city of Nazareth, to Judea, to the city of David,\n"
+                            "which is called Bethlehem,\n"
+                            "because he was of the house and lineage of David,\n"
+                            "to be enrolled with Mary, his betrothed, who was with child.\n"
+                            "And while they were there,\n"
+                            "the time came for her to be delivered.\n"
+                            "And she gave birth to her first-born son\n"
+                            "and wrapped him in swaddling cloths,\n"
+                            "and laid him in a manger,\n"
+                            "because there was no place for them in the inn.\n"
+                            "(Lk 2:1-7).\n\n\n"
+
+                            "Our Father...\n"
+                            "10 Hail Marys\n"
+                            "Glory be to the Father...\n"
+                            "Fatima Prayer...",
+                        contentAm: "እመቤታችን\n"
+                            "ጌታችን ኢየሱስ ክርስቶስን የምትወልድበት ቀን በደረሰ ጊዜ\n"
+                            "በቤተልሔም በከብቶች በረት መውለድዋን ማሰብ ነው::\n"
+                            "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                            "የዚህ ዓለም ሀብት መናቅና ድህነትን መቻል ነው፡፡\n\n\n"
+
+                            "አባታችን ሆይ...\n"
+                            "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                            "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                            "የፋጢማ ጸሎት...\n",
+                        imagePath: "assets/img_34.png"
+                    ),
+
+                    PrayerStep(
+                        titleEn: "Fourth Joyful Mystery",
+                        titleAm: "አራተኛ የደስታ ምስጢር",
+                        contentEn: "The Presentation in the Temple.\n\n"
+                            "\"And at the end of eight days,\n"
+                            "when he was circumcised,\n"
+                            "he was called Jesus,\n"
+                            "the name given by the angel before he was conceived in the womb.\n"
+                            "And when the time came for their purification\n"
+                            "according to the law of Moses,\n"
+                            "they brought him up to Jerusalem\n"
+                            "to present him to the Lord (as it is written in the law of the Lord,\n"
+                            "'Every male that opens the womb shall be called holy to the Lord')\n"
+                            "and to offer a sacrifice according to what is said in the law of the Lord,\n"
+                            "'a pair of turtledoves, or two young pigeons.'\n"
+                            "(Lk 2:21-24).\n\n\n"
+
+                            "Our Father...\n"
+                            "10 Hail Marys\n"
+                            "Glory be to the Father...\n"
+                            "Fatima Prayer...",
+                        contentAm: "እመቤታችን ማርያም\n"
+                            "ጌታችን ከወለደች በኋላ ወደ ቤተመቅደስ ወስዳ\n"
+                            "ቅዱስ ስምኦንም እርሱን በብዙ ደስታ መቀበሉን ማሰብ ነው::\n"
+                            "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                            "የእግዚአብሔርንና የቤተክርቲያንን ደንብ መጠበቅ ነው፡፡\n\n\n"
+
+                            "አባታችን ሆይ...\n"
+                            "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                            "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                            "የፋጢማ ጸሎት...\n",
+                        imagePath: "assets/img_35.png"
+                    ),
+
+                    PrayerStep(
+                        titleEn: "Fifth Joyful Mystery",
+                        titleAm: "አምስተኛ የደስታ ምስጢር",
+                        contentEn: "The finding of Jesus in the temple.\n\n"
+                            "\"Now his parents went to Jerusalem every year\n"
+                            "at the feast of the Passover.\n"
+                            "And when he was twelve years old,\n"
+                            "they went up according to custom;\n"
+                            "and when the feast was ended,\n"
+                            "as they were returning,\n"
+                            "the boy Jesus stayed behind in Jerusalem. His parents did not know it ...\n\n"
+                            "After three days they found him in the temple,\n"
+                            "sitting among the teachers,\n"
+                            "listening to them and asking them questions;\n"
+                            "and all who heard him were amazed at his understanding and his answers\n"
+                            "(Lk 2:41-47).\n\n\n"
+
+                            "Our Father...\n"
+                            "10 Hail Marys\n"
+                            "Glory be to the Father...\n"
+                            "Fatima Prayer...",
+                        contentAm: "ጌታችን ኢየሱስ\n"
+                            "በተወለደ በአሥራ ሁለት ዓመቱ\n"
+                            "እመቤታችን ማርያም ሳታውቅ በኢየሩሳሌም\n"
+                            "የእግዚአብሔር ህግ በሚያስተምሩት መካከል መገኘቱን ማሰብ ነው::\n"
+                            "የምስጢሩ ፍሬ ሐሳብ፡\n"
+                            "የእግዚአብሔርን ሕግ መማር ነው፡፡\n\n\n"
+
+                            "አባታችን ሆይ...\n"
+                            "(10) ጸጋ የሞለሽ ማርያም ሆይ...\n"
+                            "ለአብና ለወልድ ለመንፋስ ቅዱስ...\n"
+                            "የፋጢማ ጸሎት...\n",
+                        imagePath: "assets/img_36.png"
+                    )
+
                   ];
 
                   // 2. Pass the list and current language setting into prayer_session
